e7ab61b9b1bbee4e386bbfb025f5b6bcc054d640
naveena <naveenaharidas@gmail.com>
2023-10-03 13:41:52 +0530

    added

---
+++ todoproject/todoapp/urls.py
@@ -0,0 +1,12 @@
+from . import views
+from django.urls import path
+
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('delete/<int:taskid>/',views.delete,name='delete'),
+    path('update/<int:id>/',views.update,name='update'),
+    # path('details',views.details,name='details')
+
+
+]
